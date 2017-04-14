88038df7b4a25c084c13203218a44ee9ed285794
wzulfikar_mba <me@wzulfikar.com>
2017-04-14 13:48:44 +0800

    using FortDigitalSmsProvider for testing

--- test.php
+++ test.php
@@ -1,13 +1,22 @@
 <?php
 
-require __DIR__ . '/Sms.php';
+// require __DIR__ . '/Sms.php';
+require __DIR__ . '/providers/FortDigitalSmsProvider.php';
 
 $config = [
-    'base_url' => 'https://mx.fortdigital.net',
     'sender' => 'put-sender-here',
     'username' => 'your-username-here',
     'password' => 'your-password-here',
 ];
-$sms = new Sms();
+
+$class = 'FortDigitalSmsProvider';
+$interface = 'SmsInterface';
+
+if (!(new ReflectionClass($class))->implementsInterface($interface)) {
+    throw new Exception("$class must implemenst $interface");
+}
+
+$sms = new $class;
 $sms->config($config);
-echo $sms->getStatus(2332);
+var_dump($sms->getStatus(2332));
+var_dump($sms->getBalance());
