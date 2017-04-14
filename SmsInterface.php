f5fce3b084ef10b58879ddd200db41f1ae8d23b9
wzulfikar_mba <me@wzulfikar.com>
2017-04-14 13:48:09 +0800

    added sms interface

---
+++ SmsInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+interface SmsInterface
+{
+    /**
+     * Send message to given phone number
+     * @param  string $message message that will be sent
+     * @param  string $phone   recipient's phone number
+     * @return array           status of message and its id
+     */
+    public function send($message, $to_phone);
+    
+    /**
+     * Set configuration of sms driver
+     *
+     * @param  array  $opts array of config/option
+     * @return void
+     */
+    public function config(array $opts);
+
+    /**
+     * Get status of a message
+     *
+     * @param  mixed $message_id  message id from provider
+     * @return array              array representation of message's status
+     */
+    public function getStatus($message_id);
+
+    /**
+     * Get balance of sms from provider
+     *
+     * @return array    array of username & its balance
+     */
+    public function getBalance();
+}
